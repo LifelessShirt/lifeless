4e4d350d7b9efe1884f9bf8ff5575dabcd32ea89
LifelessShirt <yuuurko@gmail.com>
2024-01-14 18:40:04 +0100

    Language changer fix

--- languages.js
+++ languages.js
@@ -7,7 +7,7 @@ function setLanguage(locale) {
             $(".uppertitle").html(data[0][locale][0].uppertitle);
             $(".title").html(data[0][locale][0].title);
             $(".undertitle").html(data[0][locale][0].undertitle);
-            console.log( "Language changed succesfully" );
+            console.log( "Language changed to: "+locale );
     });
 }
 
@@ -15,27 +15,39 @@ function setLanguage(locale) {
 //
 // Better use (navigator.userLanguage or navigator.language) checking,
 // because some old browsers/devices will not support (navigator.language)
-var userLanguage = (navigator.language || navigator.userLanguage).split("-", 1); 
-switch (userLanguage) {
-  // Ukrainian language
-  case 'uk':
+const userLang = String((navigator.language || navigator.userLanguage).split("-", 1)); 
+switch (userLang) {
+  case "uk": {
+    // Ukrainian language
     setLanguage("uk");
-  // Polish language
-  case 'pl':
+    break;
+  }
+  case "pl": {
+    // Polish language
     setLanguage("pl");
-  // English language for other
-  default:
+    break;
+  }
+  case "ru": {
+    // Russian language
+    setLanguage("ru");
+    break;
+  }
+  default: {
+    // English language for other
     setLanguage("en");
+  }
 }
+console.log( "User language is '"+userLang+"'" );
 
 // Changing language by clicking a (.lang-button) buttons.
-// Also this function include animations
+// Also this function include animations.
+// Buttons are not visible on web-site cause of site has auto-switching languages above.
 $(".lang-button").click(function() {
     // removeClass will hide all element on site
     $('.content').removeClass('loaded');
     $('.logo').removeClass('loaded');
     // Get attribute (lang) from buttons
-    var selectedLang = $(this).attr("lang");
+    const selectedLang = $(this).attr("lang");
     // Timeout function is needed to wait for all elements will be hidden.
     setTimeout(function() {
         setLanguage(selectedLang);
